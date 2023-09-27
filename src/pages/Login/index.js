0e3d7adceb088f9103c91e6a405598b2142ddf06
Olanrewaju A. Olaboye <eyiwumi.olaboye@gmail.com>
2023-09-27 13:36:33 +0100

    panwine panthera

---
+++ src/pages/Login/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "../Login/login.css";
+import { Link } from "react-router-dom";
+function Login() {
+  return (
+    <div className="login">
+      <div className="bg-div">
+        {/* <p className="bg-para">PanWines</p> */}
+        <img src="../../../images/login-image.png" alt="banner" />
+      </div>
+      <div>
+        <p>Sign in to PanWines</p>
+        <form>
+          <input type="email" placeholder="Email Address" name="email" />
+          <input type="password" placeholder="Password" name="password" />
+          <button type="submit">Sign in</button>
+          <div>
+            <p>
+              Don't have an account? <Link to={"/signup"}>Sign up</Link>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/router/router.js
+++ src/router/router.js
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/signup", element: <SignUp /> },
+    ],
   },
 ]);
